45e3473543e62d3d6cb82a9459a3285020fb26b5
sukij827-max <sukij827@gmail.com>
2026-08-25 07:53:53 +0700

    rebuild: single dispatcher runtime

--- bot/runtime.py
+++ bot/runtime.py
@@ -2,5 +2,7 @@ from aiogram import Bot,Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config import settings
+from bot.handlers import router
 bot=Bot(settings.bot_token,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp=Dispatcher()
+dp.include_router(router)
